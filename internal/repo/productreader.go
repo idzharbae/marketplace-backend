b5bb75683e48dc5ff29fe412f85917452d0ee6e1
muhammad idzhar darissalam <idzharbae@gmail.com>
2020-02-17 11:37:21 +0700

    product gets repo layer, and add slug to converter

--- internal/repo/productreader.go
+++ internal/repo/productreader.go
@@ -18,14 +18,27 @@ func NewProductReader(db connection.Gormw) *ProductReader {
 
 func (p *ProductReader) List(req requests.ListProduct) ([]entity.Product, error) {
 	var products []model.Product
-	p.db.Order("id desc").Find(&products)
+	err := p.db.Order("id desc").Find(&products).Error()
+	if err != nil {
+		return nil, err
+	}
 	return converter.ProductModelsToEntities(products), nil
 }
 
 func (p *ProductReader) GetByID(productID int32) (entity.Product, error) {
-	return entity.Product{}, nil
+	var product model.Product
+	err := p.db.Where("id=?", productID).First(&product).Error()
+	if err != nil {
+		return entity.Product{}, err
+	}
+	return converter.ProductModelToEntity(product), nil
 }
 
 func (p *ProductReader) GetBySlug(slug string) (entity.Product, error) {
-	return entity.Product{}, nil
+	var product model.Product
+	err := p.db.Where("slug=?", slug).First(&product).Error()
+	if err != nil {
+		return entity.Product{}, err
+	}
+	return converter.ProductModelToEntity(product), nil
 }
