3981ab2ff0e800d0b9d6840d0a82dae8c191f9f5
idzharbae <idzharbae@gmail.com>
2020-04-16 20:58:00 +0700

    fix image folder path and add run-rest command

--- svc/resources/internal/delivery/rest/main.go
+++ svc/resources/internal/delivery/rest/main.go
@@ -3,13 +3,22 @@ package rest
 import (
 	"log"
 	"net/http"
+	"os"
+	"path"
 )
 
 func Start(port string) {
+	e, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	imgPath := path.Dir(e) + "/public/img/"
+	log.Println(imgPath)
+
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./svc/resources/public/img/"))
+	fileServer := http.FileServer(http.Dir(imgPath))
 	mux.Handle("/img/", http.StripPrefix("/img/", fileServer))
 	log.Println("Starting server on " + port)
-	err := http.ListenAndServe(port, mux)
+	err = http.ListenAndServe(port, mux)
 	log.Fatal(err)
 }
