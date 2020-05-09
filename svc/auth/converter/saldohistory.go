8843cd3fb1aa69105e693ef962975716d99db666
idzharbae <idzharbae@gmail.com>
2020-05-09 19:46:19 +0700

    repo layer

--- svc/auth/converter/saldohistory.go
+++ svc/auth/converter/saldohistory.go
@@ -3,6 +3,7 @@ package converter
 import (
 	"github.com/idzharbae/marketplace-backend/svc/auth/authproto"
 	"github.com/idzharbae/marketplace-backend/svc/auth/internal/entity"
+	"github.com/idzharbae/marketplace-backend/svc/auth/internal/repo/model"
 )
 
 func SaldoHistoryProtoToEntity(in *authproto.SaldoHistory) entity.SaldoHistory {
@@ -31,6 +32,25 @@ func SaldoHistoryEntitiesToProtos(req []entity.SaldoHistory) []*authproto.SaldoH
 	histories := make([]*authproto.SaldoHistory, len(req))
 	for i, history := range req {
 		histories[i] = SaldoHistoryEntityToProto(history)
+	}
+	return histories
+}
+
+func SaldoHistoryModelToEntity(req model.SaldoHistory) entity.SaldoHistory {
+	return entity.SaldoHistory{
+		ID:           req.ID,
+		UserID:       req.UserID,
+		SourceID:     req.SourceID,
+		Description:  req.Description,
+		ChangeAmount: req.ChangeAmount,
+		CreatedAt:    req.CreatedAt,
+		UpdatedAt:    req.UpdatedAt,
+	}
+}
+func SaldoHistoryModelsToEntities(req []model.SaldoHistory) []entity.SaldoHistory {
+	histories := make([]entity.SaldoHistory, len(req))
+	for i, history := range req {
+		histories[i] = SaldoHistoryModelToEntity(history)
 	}
 	return histories
 }
