b1731fea454f0f751de8fc275df005108f8f9543
idzharbae <idzharbae@gmail.com>
2020-05-09 20:28:32 +0700

    update saldo history when topup or buying product

--- svc/auth/internal/usecase/payment.go
+++ svc/auth/internal/usecase/payment.go
@@ -9,11 +9,12 @@ import (
 )
 
 type Payment struct {
-	userWriter internal.UserWriter
+	userWriter         internal.UserWriter
+	saldoHistoryWriter internal.SaldoHistoryWriter
 }
 
-func NewPaymentUC(writer internal.UserWriter) *Payment {
-	return &Payment{userWriter: writer}
+func NewPaymentUC(writer internal.UserWriter, shw internal.SaldoHistoryWriter) *Payment {
+	return &Payment{userWriter: writer, saldoHistoryWriter: shw}
 }
 
 // exposed to graphql so need validation
@@ -21,10 +22,24 @@ func (p *Payment) TopUp(req request.TopUp) (entity.User, error) {
 	if req.Amount < 0 {
 		return entity.User{}, errors.New("topup amount cant be negative")
 	}
-	return p.userWriter.UpdateSaldo(req)
+	res, err := p.userWriter.UpdateSaldo(req)
+	if err != nil {
+		return entity.User{}, err
+	}
+	p.saldoHistoryWriter.Create(entity.SaldoHistory{
+		UserID:       req.UserID,
+		Description:  "topup",
+		ChangeAmount: req.Amount,
+	})
+	return res, nil
 }
 
 func (p *Payment) UpdateSaldo(req request.TopUp) (entity.User, error) {
+	p.saldoHistoryWriter.Create(entity.SaldoHistory{
+		UserID:       req.UserID,
+		Description:  "payment",
+		ChangeAmount: req.Amount,
+	})
 	return p.userWriter.UpdateSaldo(req)
 }
 
@@ -33,4 +48,13 @@ func (p *Payment) Transfer(req request.Transfer) (authproto.TransferSaldoResp, e
 		return authproto.TransferSaldoResp{}, errors.New("transfer amount cant be negative")
 	}
 	return p.userWriter.TransferSaldo(req)
+}
+
+func (p *Payment) saveSaldoHistory(userID, amount int64) error {
+	_, err := p.saldoHistoryWriter.Create(entity.SaldoHistory{
+		UserID:       userID,
+		Description:  "topup",
+		ChangeAmount: amount,
+	})
+	return err
 }
